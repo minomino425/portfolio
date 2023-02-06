57e363a39bfa958aa3752f66cdff6ccc7845eb09
蓑田朋未 <tomomi.minoda425@gmail.com>
2023-02-07 02:24:39 +0900

    update

--- components/comment.js
+++ components/comment.js
@@ -11,9 +11,11 @@ export default function Comment({
     ? "ノイズでざらつきのあるような質感を表現しました。時間経過またはマウスカーソルを動かすと色が変化します。ただのベタ塗りより繊細な見え方になる気がします。"
     : "";
   const glassComment = glass
-    ? "すりガラスのような見た目になりながら画像が切り替わるスライダーです。横方向に少しずらしながら切り替えることで、より自然に見えるようにしました。イージングや秒数などの指定にはGSAPを使っています。高級感を演出したいサイトのFVなどで使用すると、目を引く演出になるのではと思います。"
+    ? "すりガラスのような見た目になりながら画像が切り替わるスライダーです。横方向に少しずらしながら切り替えることで、より自然に見えるようにしました。イージングや秒数などの指定にはGSAPを使っています。"
     : "";
-  const morphComment = morph ? "トーラスをモーフィングさせました。マテリアルはトゥーンレンダリングと呼ばれるアニメ風で陰影がはっきりしている手法を使用しています。" : "";
+  const morphComment = morph
+    ? "トーラスをモーフィングさせました。マテリアルはトゥーンレンダリングと呼ばれるアニメ風で陰影がはっきりしている手法を使用しています。"
+    : "";
   const noiseSlideComment = noiseSlide
     ? "ノイズを使って水玉模様に切り替わるスライダーです。"
     : "";
@@ -22,20 +24,43 @@ export default function Comment({
   const classToggle = () => {
     setActive(!active);
   };
+  const refText = useRef(null);
+  const refTitle = useRef(null);
+
+  const style = {
+    "--title-width": refTitle.current
+      ? `${refTitle.current.offsetWidth}px`
+      : "130px",
+    "--title-height": refTitle.current
+      ? `${refTitle.current.offsetHeight}px`
+      : "27px",
+    "--text-width": refText.current
+      ? `${refText.current.offsetWidth}px`
+      : "0px",
+    "--text-height": refText.current
+      ? `${refText.current.offsetHeight}px`
+      : "0px",
+  };
+
   return (
     <>
       <a
-        className={`${styles.comment} ${active ? styles.active : ""}`}
+        className={`${styles.comment} ${active ? styles.open : styles.close}`}
         onClick={classToggle}
+        ref={refTitle}
+        {...{ style }}
       >
         <span className={styles["comment-title"]}>Comment</span>
-        <div class="c-coder__popup__bg"></div>
       </a>
       <a
-        className={`${styles["comment-wrap"]} ${active ? styles.active : ""}`}
+        className={`${styles["comment-wrap"]} ${
+          active ? styles.open : styles.close
+        }`}
         onClick={classToggle}
+        ref={refText}
+        {...{ style }}
       >
-       <p className={styles.commentText}>
+        <p className={styles.commentText}>
           {noiseBgComment && noiseBgComment}
           {glassComment && glassComment}
           {morphComment && morphComment}
