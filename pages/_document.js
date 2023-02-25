13511b016e45c19fbda764f1b6c37d652f664c6a
蓑田朋未 <tomomi.minoda425@gmail.com>
2023-02-25 22:14:57 +0900

    update

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Syne:wght@400;500;600;700;800&&family=Karla&display=swap&&family=Noto+Sans+JP&family=Unbounded:wght@900&family=Fjalla+One&display=swap"
