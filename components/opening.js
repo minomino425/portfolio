4e46928de3c11b4684fa2215df4336b602043787
蓑田朋未 <tomomi.minoda425@gmail.com>
2023-02-27 01:45:05 +0900

    update

--- components/opening.js
+++ components/opening.js
@@ -2,30 +2,67 @@ import styles from "../styles/opening.module.css";
 import { gsap } from "gsap";
 import React, { useState, useEffect, useRef } from "react";
 import NextImage from "next/image";
-import url from "../public/opening.png";
+import url1 from "../public/opening_1.png";
+import url2 from "../public/opening_2.png";
+import url3 from "../public/opening.png";
 
 export default function Opening() {
   const tl = gsap.timeline({});
 
   useEffect(() => {
-    tl.to("#box", {
-      scaleY: 1,
-      duration: 0.5,
-      ease: "power3.inOut",
-    });
-    tl.to("#box", {
-      scaleY: 0,
-      duration: 0.5,
-      ease: "power3.inOut",
-      transformOrigin: "top",
-    });
+    tl.to(
+      `.${styles["image1"]}`,
+      {
+        autoAlpha: 0,
+      },
+      "1"
+    );
+    tl.to(
+      `.${styles["image2"]}`,
+      {
+        autoAlpha: 1,
+      },
+      "<"
+    );
+    tl.to(
+      `.${styles["image2"]}`,
+      {
+        autoAlpha: 0,
+      },
+      "<1"
+    );
+    tl.to(
+      `.${styles["image3"]}`,
+      {
+        autoAlpha: 1,
+      },
+      "<"
+    );
   }, []);
 
   return (
     <>
       <section className={styles["opening"]}>
         <div className={styles["opening-inner"]}>
-          <NextImage className="object-contain" src={url} alt="" />
+          <NextImage
+            className={`${styles["opening-image"]} ${styles["image1"]}`}
+            src={url1}
+            alt=""
+            priority
+          />
+          <NextImage
+            className={`${styles["opening-image"]} ${styles["image2"]}`}
+            src={url2}
+            alt=""
+            priority
+          />
+          <NextImage
+            className={`${styles["opening-image"]} ${styles["image3"]}`}
+            src={url3}
+            alt=""
+            priority
+          />
+          <div className={styles.block}></div>
         </div>
       </section>
     </>
